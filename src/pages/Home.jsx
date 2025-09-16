29ff00c8e6a654a5952f32a7de7eb995082b7ed2
RohitChintalapudi <rohit.chintalapudi.work@gmail.com>
2025-09-16 14:59:40 +0530

    added download resume button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
+
   return (
     <main className="-mt-15 ml-6">
       <motion.div
@@ -11,26 +12,59 @@ export default function Home() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
-        <motion.p 
-        initial={{ opacity: 0,opacity: 0 }}
-        animate={{ opacity: 1, opacity: 1 }}
-        transition={{ duration: 1, ease: "easeOut" }} className="font-extrabold text-[47px] relative">Front End Developer</motion.p>
-        <p className="relative font-bold text-[18px] w-[700px] ">
-         <b className="bg-purple-500"> I am Rohit - web developer </b> with a passion for creating beautiful and responsive websites. I specialize in transforming ideas into <b className="bg-cyan-400">interactive and user-friendly</b> digital experiences.
-        </p>
-        <motion.button
-         initial={{ scale: 0 }} animate={{ scale: 1 }}
-           whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           transition={{ duration: 1, ease: "easeOut" }}
-          className="outline-2 w-30 p-2 inline-block rounded-xl backdrop-blur-md shadow-2xl hover:shadow-purple-500 bg-white/30 font-bold relative
-          top-4 hover:backdrop-blur-2xl hover:shadow-2xl hover:bg-gradient-to-br from-blue-700 via-violet-700 to-fuchsia-700 bg-opacity-80 cursor-pointer
-          text-white drop-shadow-[0_0_20px_rgba(147,51,234,1)]"
-          onClick={() => {navigate("/contact");}}
+          className="font-extrabold text-[47px] relative"
         >
-          Hire me!
-        </motion.button>
+          Front End Developer
+        </motion.p>
+
+        <p className="relative font-bold text-[18px] w-[700px] ">
+          <b className="bg-purple-500"> I am Rohit - web developer </b> with a
+          passion for creating beautiful and responsive websites. I specialize
+          in transforming ideas into{" "}
+          <b className="bg-cyan-400">interactive and user-friendly</b> digital
+          experiences.
+        </p>
+
+        <div className="flex gap-4">
+          <motion.button
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="outline-2 w-30 p-2 inline-block rounded-xl backdrop-blur-md shadow-2xl hover:shadow-purple-500 bg-white/30 font-bold relative
+              top-4 hover:backdrop-blur-2xl hover:shadow-2xl hover:bg-gradient-to-br from-blue-700 via-violet-700 to-fuchsia-700 bg-opacity-80 cursor-pointer
+              text-white drop-shadow-[0_0_20px_rgba(147,51,234,1)]"
+            onClick={() => {
+              navigate("/contact");
+            }}
+          >
+            Hire me!
+          </motion.button>
+          {/* Resume Button */}
+          <motion.a
+            href="/assets/My_resume.pdf"
+            download="Rohit_Chintalapudi_Resume.pdf"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="outline-2 w-30 p-2 inline-block rounded-xl backdrop-blur-md shadow-2xl 
+           hover:shadow-purple-500 bg-white/30 font-bold relative top-4
+          hover:backdrop-blur-2xl hover:shadow-2xl 
+          hover:bg-gradient-to-br from-blue-700 via-violet-700 to-fuchsia-700 bg-opacity-80 
+          cursor-pointer text-white drop-shadow-[0_0_20px_rgba(147,51,234,1)] text-center"
+          >
+            Download Resume
+          </motion.a>
+        </div>
       </motion.div>
+
       <motion.div
         className="flex justify-end relative mr-[230px] mt-9"
         initial={{ opacity: 0, x: 50 }}
@@ -41,7 +75,7 @@ export default function Home() {
           src="../assets/1749228827951.jpg"
           className="rounded-full border-2 border-white shadow-[0_0_15px_rgb(0,0,0)] fixed scale-165"
           initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2, ease: "easeOut" }}
         />
       </motion.div>
